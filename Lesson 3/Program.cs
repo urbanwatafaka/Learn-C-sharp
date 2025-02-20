fb671618bfeb5cd04317b32285f93f4e66e5edf7
Антон <hlopchegg@gmail.com>
2025-02-20 18:50:50 +0300

    Использование ввода в консоли

---
+++ Lesson 3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+//Ввод данных в консоль
+
+namespace Lesson_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string data;
+            data = Console.ReadLine();
+            Console.WriteLine("Привет " + data);
+            Console.ReadKey();
+        }
+    }
+}
