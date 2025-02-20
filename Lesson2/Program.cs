1f8527774f42c6eccca20b396aa1cb5ebd2008b7
Антон <hlopchegg@gmail.com>
2025-02-20 18:30:30 +0300

    Урок по объявлению и инициализации переменных.

--- Lesson2/Program.cs
+++ Lesson2/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel.Design.Serialization;
+/* Переменыне в C#
+ * 1. Объявление (создание) переменной
+ * 2. Инициализация переменной
+ * 3. Присвоение значение переменной
+*/
 
 namespace Lesson2
 {
@@ -10,6 +12,18 @@ namespace Lesson2
     {
         static void Main(string[] args)
         {
+            //Объявляем переменную: Тип_данных имя_переменной
+            int a, b;
+
+            //Инициализация переменной: к переменной присваиваем значение (a = 1)
+            a = 10;
+            b = 5;
+            Console.WriteLine(a + b);
+
+            //Так же можно инициализировать переменную во время ее создания;
+            //int a = 10;
+
+            //Console.WriteLine(a);
         }
     }
 }
