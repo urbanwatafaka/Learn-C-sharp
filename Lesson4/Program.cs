967ee3d2d73f98bf3fa52358ea753bf6c96103f6
Антон <hlopchegg@gmail.com>
2025-02-21 13:36:21 +0300

    Урок по конвертации строки в число

---
+++ Lesson4/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Введите первое число");
+            //string a = Console.ReadLine();
+            //int a1;
+            //a1 = Convert.ToInt32(a);
+            //Console.WriteLine("Введите второе чиcло");
+            //string b = Console.ReadLine();
+            //int b1;
+            //b1 = Convert.ToInt32(b);
+            //Console.WriteLine("Сумма чисел равна: ");
+            //Console.WriteLine(a1 + b1);
+            //ИЛИ
+            string str;
+            int a, b;
+            Console.WriteLine("Введите первое число");
+            str = Console.ReadLine();
+            a = Convert.ToInt32(str);
+            Console.WriteLine("Введите второе число");
+            str = Console.ReadLine();
+            b = Convert.ToInt32(str);
+            int result = a + b;
+            Console.WriteLine("Сумма чисел = " + result);
+            
+        }
+    }
+}
