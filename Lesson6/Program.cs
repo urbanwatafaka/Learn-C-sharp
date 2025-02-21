f6c715203c90e22562abacf22f53dc19ede2a24e
Антон <hlopchegg@gmail.com>
2025-02-21 18:06:38 +0300

    Уроки по преобразованию int -> double(с плавающей точкой)

---
+++ Lesson6/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+
+//Конвертация строки;
+//parse и tryparse
+
+namespace Lesson6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string str = "0.9";
+            //int a = int.Parse(str);
+            //Console.WriteLine(a);
+
+
+
+
+            //string str = "5,9";
+            //NumberFormatInfo numberFormatInfo = new NumberFormatInfo()
+            //{
+            //    NumberDecimalSeparator = ".",
+            //};
+            //double a = double.Parse(str, numberFormatInfo);
+            //Console.WriteLine(a);
+
+
+            string str = "1dsf";
+            int a;
+            bool result = int.TryParse(str, out a);
+            Console.WriteLine(result);
+            if (result)
+            {
+                Console.WriteLine("Удалось конвертировать, результат: " + a);
+            }
+            else
+            {
+                Console.WriteLine("Не удалось конвертировать");
+            }
+        }
+    }
+}
