2ab6d19f02e11403f706ccbbb3aaced8864ee9ee
Антон <hlopchegg@gmail.com>
2025-02-21 13:55:06 +0300

    Конвертация, если региональные настройки устройств отличаются

---
+++ Lesson5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+/* Конвертация, если региональные настройки устройств отличаются. 
+Например: дробное число через ТОЧКУ (1.9), а не запятую (1,9)
+*/
+
+namespace Lesson5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "1.9";
+
+            NumberFormatInfo numberFormatInfo = new NumberFormatInfo()
+            {
+                NumberDecimalSeparator = ".",
+            };
+
+            double a = Convert.ToDouble(str, numberFormatInfo);
+            Console.WriteLine(a);
+        }
+    }
+}
