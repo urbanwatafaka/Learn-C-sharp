ef84ead81bedb9d7bd0f61a33aa8d1a368927965
Антон <hlopchegg@gmail.com>
2025-02-25 19:03:08 +0300

    Операторы, арифметические операции с числами

---
+++ HomeWork Lesson7/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Поиск среднего арифметического
+namespace HomeWork_Lesson7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите 2 числа для нахождения среднего арифметического");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            double result = (double)(a + b) / 2;
+            Console.WriteLine("Среднее арифметическое = "+result);
+
+        }
+    }
+}
